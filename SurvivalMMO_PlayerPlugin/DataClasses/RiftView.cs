fc9589f03fc3f4e1a2660e921c11709bf2dcd2ab
jlomba <johnlomba1993@gmail.com>
2023-06-03 20:27:04 -0400

    starting rift view
    had to move sever to inside project to keep local reference while building

---
+++ SurvivalMMO_PlayerPlugin/DataClasses/RiftView.cs
@@ -0,0 +1,59 @@
+﻿using DarkRift;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurvivalMMO_PlayerPlugin
+{
+    /// <summary>
+    ///     Holds serializable data about a player.
+    /// </summary>
+    public class RiftView : IDarkRiftSerializable
+    {
+        public ushort ID { get; set; }
+        public ushort Owner { get; set; }
+
+        public RiftView(ushort id, ushort owner)
+        {
+            ID = id;
+            Owner = owner;
+        }
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            this.ID = e.Reader.ReadUInt16();
+            this.Owner = e.Reader.ReadUInt16();
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(ID);
+            e.Writer.Write(Owner);
+        }
+
+        /// <summary>
+        ///     Compares an object for equality with this.
+        /// </summary>
+        /// <param name="obj">The object to compare with.</param>
+        /// <returns>Whether the object is equal to this block.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is RiftView))
+                return false;
+
+            RiftView b = (RiftView)obj;
+
+            return this.ID == b.ID && this.Owner == b.Owner;
+        }
+        /// <summary>
+        ///     Basic hashcode generator based on the object id
+        /// </summary>
+        /// <returns>hash code.</returns>
+        public override int GetHashCode()
+        {
+            return (int)(this.ID * 23 + this.Owner * 29);
+        }
+    }
+}
---
+++ SurvivalMMO_PlayerPlugin/ServerPlayerManager.cs
@@ -0,0 +1,69 @@
+﻿using DarkRift;
+using DarkRift.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurvivalMMO_PlayerPlugin
+{
+    internal class ServerPlayerManager : Plugin
+    {
+        public override bool ThreadSafe => true;
+
+        public override Version Version => new Version(1,0,0);
+
+        public ServerPlayerManager(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+            //Subscribe for notification when a new client connects
+            ClientManager.ClientConnected += ClientManager_ClientConnected;
+
+            //Subscribe for notifications when a new client disconnects
+            ClientManager.ClientDisconnected += ClientManager_ClientDisconnected;
+        }
+
+
+        Dictionary<IClient, RiftView> players = new Dictionary<IClient, RiftView>();
+
+        void ClientManager_ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            //Subscribe to when this client sends messages
+            e.Client.MessageReceived += Client_PlayerMessageReceivedEvent;
+
+            RiftView newConnection = new RiftView(e.Client.ID, e.Client.ID);
+
+            lock (players)
+                players.Add(e.Client, newConnection);
+
+            using (Message message = Message.Create(MessageTag.PlayerConnected, newConnection))
+            {
+                foreach (IClient sendTo in ClientManager.GetAllClients().Except(new IClient[] { e.Client }))
+                {
+                    e.Client.SendMessage(message, SendMode.Reliable);
+                }
+            }
+        }
+
+        private void ClientManager_ClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            RiftView player = new RiftView(e.Client.ID, e.Client.ID);
+
+            lock (players)
+                players.Remove(e.Client);
+
+            using (Message message = Message.Create(MessageTag.PlayerDisconnected, player))
+            {
+                foreach (IClient sendTo in ClientManager.GetAllClients().Except(new IClient[] { e.Client }))
+                {
+                    e.Client.SendMessage(message, SendMode.Reliable);
+                }
+            }
+        }
+
+        void Client_PlayerMessageReceivedEvent(object sender, MessageReceivedEventArgs e)
+        {
+
+        }
+    }
+}
